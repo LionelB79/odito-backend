3d9ab09ecb3a854391b3aeb31024d5b6e3c01013
Lionel <bachmannlionel@gmail.com>
2025-06-22 19:09:19 +0200

    ajout 1 test

---
+++ src/test/java/com/odito/odito_backend/OditoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odito.odito_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OditoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
