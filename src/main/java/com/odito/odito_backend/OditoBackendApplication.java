28739c1a0f2ba914e08da50d8fde6ec6374f1de9
Lionel <bachmannlionel@gmail.com>
2025-06-22 19:09:12 +0200

    renommage et gitignore

---
+++ src/main/java/com/odito/odito_backend/OditoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.odito.odito_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OditoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OditoBackendApplication.class, args);
+	}
+
+}
